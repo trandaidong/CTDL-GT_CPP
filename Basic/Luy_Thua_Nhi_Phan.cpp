bd998436d22d257889e0a2b2e146e568426e5821
Trần Đại Đồng <120238379+trandaidong@users.noreply.github.com>
2023-08-08 22:22:44 +0700

    Update Luy_Thua_Nhi_Phan.cpp

--- Basic/Luy_Thua_Nhi_Phan.cpp
+++ Basic/Luy_Thua_Nhi_Phan.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+// Hàm tính lũy thừa nhị phân bằng đệ quy
+long long binpowRecursive(int a,int b){
+  
+}
+long long binpow(int a,int b){
+    long long res=1;
+    while(b!=0){
+        if(b%2==1){
+            res*=a;
+        }
+        b/=2;
+        a*=a;
+    }
+    return res;
+}
+int main() {
+    cout << binpow(2,5);
+    return 0;
+}
