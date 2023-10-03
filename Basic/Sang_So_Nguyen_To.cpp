64a728ab5c1edf0d1af64833bd152e030ffd8f90
Trần Đại Đồng <120238379+trandaidong@users.noreply.github.com>
2023-10-03 21:33:21 +0700

    Create Sang_So_Nguyen_To.cpp

---
+++ Basic/Sang_So_Nguyen_To.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+bool solve[1000001];
+int n_max = 10000001;
+void sang() {
+	for (int i = 0; i <= n_max; i++) {
+		solve[i] = true;
+	}
+	solve[0] = solve[1] = false;
+	for (int i = 2; i <= sqrt(n_max); i++) {
+		if (solve[i]) {
+			for (int j = i * i; j <= n_max; j += i) {
+				solve[j] = false;
+			}
+		}
+	}
+}
+int main() {
+	// sang so nguyen to
+	sang();
+	for (int i = 0; i <= 1000; i++) {
+		if (solve[i])cout << i << " ";
+	}
+	return 0;
+}
