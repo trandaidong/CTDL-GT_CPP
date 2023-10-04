8c861c9d1e7dc4c7c4660329fbb42bb592c2bc61
Trần Đại Đồng <120238379+trandaidong@users.noreply.github.com>
2023-10-04 10:02:09 +0700

    Create Legendre_Formula.cpp

---
+++ Basic/Legendre_Formula.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int legendre1(int n,int k){
+    int count=0;
+    for(int i=k;i<=n;i+=k){
+        int m=i;
+        while(m%k==0){
+            count++;
+            m/=k;
+        }
+    }
+    return count;
+}
+int legendre2(int n,int k){
+    int ans=0;
+    for(int i=k;i<=n;i*=k){
+        ans+=n/i;
+    }
+    return ans;
+}
+int main() {
+    cout << legendre2(10,3);
+    return 0;
+}
