af1e0335bb282fea895aa1c9e72c8f1b01088629
Trần Đại Đồng <120238379+trandaidong@users.noreply.github.com>
2023-10-04 10:50:55 +0700

    Update Sang_So_Nguyen_To.cpp

--- Basic/Sang_So_Nguyen_To.cpp
+++ Basic/Sang_So_Nguyen_To.cpp
@@ -15,6 +15,21 @@ void sang() {
 		}
 	}
 }
+void sang(int l,int r){
+    int *prime=new int[r-l+1];
+    for(int i=0;i<=r-l;i++){
+        prime[i]=1;
+    }
+    prime[0]=prime[1]=0;
+    for(int i=2;i<=sqrt(r);i++){
+        for(int j=max(i*i,(l+i-1)/i*i);j<=r;j+=i){
+            prime[j-l]=0;
+        }
+    }
+    for(int i=max(2,l);i<=r;i++){
+        if(prime[i-l])cout<<i<<" ";
+    }
+}
 int main() {
 	// sang so nguyen to
 	sang();
