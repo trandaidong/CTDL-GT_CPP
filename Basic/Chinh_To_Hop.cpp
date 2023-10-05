a04d61688b096940d8ee189b856cd50cd7c32e67
Trần Đại Đồng <120238379+trandaidong@users.noreply.github.com>
2023-10-05 09:31:08 +0700

    Update Chinh_To_Hop.cpp

--- Basic/Chinh_To_Hop.cpp
+++ Basic/Chinh_To_Hop.cpp
@@ -14,6 +14,21 @@ long long nCk(int k,int n){
     }
     return res;
 }
+const int MOD=1000000007;
+int C[1001][1001];
+void Ckn(){
+    for(int i=0;i<=1000;i++){
+        for(int j=0;j<=i;j++){
+            if(j==0||i==j){
+                C[i][j]=1;
+            }
+            else{
+                C[i][j]=C[i-1][j-1]+C[i-1][j];// Lưu tổ hợp chập j của i
+                // C[i][j]%=MOD;
+            }
+        }
+    }
+}
 long long nAk(int k,int n){
     long long res=1;
     for(int i=0;i<k;i++){
