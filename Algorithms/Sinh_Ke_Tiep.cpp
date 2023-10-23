91d225aa0a791741790cc4060d3b27fd9f07c935
Trần Đại Đồng <120238379+trandaidong@users.noreply.github.com>
2023-10-24 06:46:29 +0700

    Update Sinh_Ke_Tiep.cpp

--- Algorithms/Sinh_Ke_Tiep.cpp
+++ Algorithms/Sinh_Ke_Tiep.cpp
@@ -1,1 +1,22 @@
-1213
+#include<iostream>
+using namespace std;
+int A[100] = { 0 };
+void BinaryBirth(int n) {
+	bool ok = false;
+	while (ok!=true) {
+		for (int i = 1; i <= n; i++)cout << A[i];
+		cout << endl;
+		int i = n;
+		while (i >= 1 && A[i] == 1) {
+			A[i] = 0;
+			--i;
+		}
+		A[i] = 1;
+		if (i < 1)ok = true;
+	}
+}
+int main() {
+	int n; cin >> n;
+	BinaryBirth(n);
+	return 0;
+}
